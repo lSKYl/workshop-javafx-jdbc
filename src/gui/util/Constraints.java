b6c48925f3146dcacfb41abc22581156ab022065
Lucas Soares <drak263@gmail.com>
2023-04-03 17:55:07 -0300

    About view

---
+++ src/gui/util/Constraints.java
@@ -0,0 +1,33 @@
+package gui.util;
+
+import javafx.scene.control.TextField;
+
+//Limitar TextField
+public class Constraints {
+	//Limita para inteiros
+	public static void setTextFieldInteger(TextField txt) {
+		txt.textProperty().addListener((obs, oldValue, newValue) -> {
+			if (newValue != null && !newValue.matches("\\d*")) {
+				txt.setText(oldValue);
+			}
+		});
+	}
+
+	//Limite maximo de caracteres
+	public static void setTextFieldMaxLength(TextField txt, int max) {
+		txt.textProperty().addListener((obs, oldValue, newValue) -> {
+			if (newValue != null && newValue.length() > max) {
+				txt.setText(oldValue);
+			}
+		});
+	}
+	
+	//Limita para apenas double
+	public static void setTextFieldDouble(TextField txt) {
+		txt.textProperty().addListener((obs, oldValue, newValue) -> {
+			if (newValue != null && !newValue.matches("\\d*([\\.]\\d*)?")) {
+				txt.setText(oldValue);
+			}
+		});
+	}
+}
